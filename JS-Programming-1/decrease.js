6819f0f701e2cf614ed56587ba71d9ef0d9e134a
Tridib11 <tridibghosh12345@gmail.com>
2024-07-27 23:27:16 +0530

    Fle added

---
+++ JS-Programming-1/decrease.js
@@ -0,0 +1,7 @@
+let i=1;
+let n=20;
+while(n>=i){
+	console.log(n);
+	n--;
+}
+
---
+++ JS-Programming-1/switchCase.js
@@ -0,0 +1,12 @@
+let name="tridib"
+switch(name){
+  case "tridib":
+    console.log("Wrokign")
+    break;
+    
+  case "rohan":
+  	console.log("Gawar")
+  	break; 
+  default:
+  	console.log("Don't know")
+}
