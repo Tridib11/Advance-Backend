b61864034ec77e415d7f0f0bf113bdafc1ced6c9
Tridib11 <tridibghosh12345@gmail.com>
2024-07-31 00:35:54 +0530

    file added

--- JS-Programming-1/typeCoersion.js
+++ JS-Programming-1/typeCoersion.js
@@ -5,3 +5,10 @@ console.log("hello"+true) //hellotrue
 console.log("2"*"2")//4
 console.log(true*true)//1
 console.log(12/"3")//4
+console.log(NaN===NaN)
+console.log(5===5)
+console.log(5==5)
+console.log(5=="5")
+console.log(1!==true)
+console.log(1!=true)
+console.log(NaN!==NaN)
