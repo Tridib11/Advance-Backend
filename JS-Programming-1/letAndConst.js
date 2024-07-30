abdcf86b16e343c6b9f7737fa123c287ace45d3b
Tridib11 <tridibghosh12345@gmail.com>
2024-07-30 22:56:09 +0530

    File added

--- JS-Programming-1/letAndConst.js
+++ JS-Programming-1/letAndConst.js
@@ -3,5 +3,28 @@ let x=10
 x=20
 console.log(x)
 
-const y=90
-y=9
+// const y=90
+// y=9    //Assignment to a constant variable
+
+
+// Block Scoping
+
+// {
+//   let nme="Tridib"
+//   console.log(nme)
+// }
+
+
+
+// console.log(nme)//nme is not defined
+
+
+
+
+{
+  // console.log(r)// it will say cannot acces X before initialization called as TDZ(Temporal Dead Zone)
+  let r=2
+  console.log(r)
+}
+
+// console.log(r) //r is not defined
