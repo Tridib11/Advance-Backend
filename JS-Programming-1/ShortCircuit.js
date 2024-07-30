d0a5956375f87f0893699bc89ef55bfb85b95d4e
Tridib11 <tridibghosh12345@gmail.com>
2024-07-30 23:54:39 +0530

    File added

--- JS-Programming-1/ShortCircuit.js
+++ JS-Programming-1/ShortCircuit.js
@@ -4,4 +4,16 @@ console.log(true||false)
 console.log(false||true)
 console.log(false||false)
 
+function left(){
+  console.log("Left")
+  return true
+}
+
+function right(){
+  console.log("right")
+  return false
+}
+
+left()||right()  //left
+
 
