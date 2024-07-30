29026e4daf19fc650bba0710337d8b8010e4cb9a
Tridib11 <tridibghosh12345@gmail.com>
2024-07-31 01:31:33 +0530

    file added

--- JS-Programming-1/bool.js
+++ JS-Programming-1/bool.js
@@ -2,3 +2,22 @@ let x=10
 console.log(!x) //false
 console.log(!undefined) //true
 
+console.log(null==false)
+
+
+
+/*
+
+x==y
+
+x->Number y->String -> converts y to a number and then compare
+x->String y->Number -> Converts x to a number and then compare
+
+x->boolean ->converts x to a number and then compare again
+
+y->boolean -> convers y to a number and then compare again
+
+x->Number,String,Symbol and y->object = we comapre y to primitive and Compare again
+
+
+*/
