22b82ab301f439f0f0841fbd75bc1e966e29150a
Tridib11 <tridibghosh12345@gmail.com>
2024-08-01 01:17:07 +0530

    File added

--- JS-Programming-1/NAN.js
+++ JS-Programming-1/NAN.js
@@ -1,4 +1,13 @@
-console.log(Number("123"))
-console.log(Number("abcd"))
-console.log(Number("0xa"))
+console.log(Number("123")) //123
+console.log(Number("abcd")) //NaN
+console.log(Number("0xa")) //10
+
+//cannot check if it is NAN or not cox NaN==NaN is false
+
+let x=NaN
+console.log(x==NaN) //false
+
+console.log(isNaN(x)) //true
+
+console.log(isNaN("tridib")) //true because it converts the value toNumber 
 
