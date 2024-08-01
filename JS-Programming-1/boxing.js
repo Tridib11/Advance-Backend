7d4a2d3e54a608175e2f6d5f6f5c4225c5d2b720
Tridib11 <tridibghosh12345@gmail.com>
2024-08-01 12:36:39 +0530

    File added

--- JS-Programming-1/boxing.js
+++ JS-Programming-1/boxing.js
@@ -1,2 +1,13 @@
 console.log(Number(1).toString())
 
+
+//Js auomaicLLY converts the variables to object beacuse of that we are bale to give them these functions like toString(),toLowerCase() all that
+
+//it boxes it in an object
+
+
+
+
+x=10
+console.log(x.toString())
+console.log((10).toString())
