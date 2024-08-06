f10d1e22cc1f486defac9d3044c219b801f9032a
Tridib11 <tridibghosh12345@gmail.com>
2024-08-06 22:51:32 +0530

    File added

---
+++ JS-Programming-1/testing.js
@@ -0,0 +1,16 @@
+var teacher="sanket"
+function fun(){
+  var teacher="tridib"
+  content=js
+  console.log(teacher)
+}
+
+function gun(){
+  var student="sarthak"
+  console.log(student)
+}
+
+console.log("starttig")
+fun()
+fun()
+console.log("ending")
