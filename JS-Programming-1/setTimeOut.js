1b21ee4360fc78470bc6f77661fcacc0828a390d
Tridib11 <tridibghosh12345@gmail.com>
2024-08-06 18:52:19 +0530

    File added

--- JS-Programming-1/setTimeOut.js
+++ JS-Programming-1/setTimeOut.js
@@ -1,8 +1,42 @@
-function x(){
-  var i=1;
-  setTimeout(function(){
-    console.log(i)
-  },3000)
-}
+// function x(){
+//   var i=1;
+//   setTimeout(function(){
+//     console.log(i)
+//   },3000)
+// }
 
+// x()
+
+function x(){
+  for(var i=1;i<=5;i++){
+    setTimeout(function (){
+      console.log(i) ///6,6,6,6,6
+    },i*1000)
+  }
+}
 x()
+
+// function x(){
+//   for(let i=1;i<=5;i++){
+//     setTimeout(function (){
+//       console.log(i) ///1,2,3,4,5
+//     },i*1000)
+//   }
+// }
+// x()
+
+function x(){
+  for(var i=1;i<=5;i++){
+    function close(i){
+      setTimeout(function (){
+        console.log(i) //1,2,3,4,5
+      },i*1000)
+    }
+    close(i)
+  }
+}
+//  x()
+
+
+
+
