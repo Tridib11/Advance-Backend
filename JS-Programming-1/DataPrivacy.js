cdf24b5329ef0549d092dfeab10f100a56622d1a
Tridib11 <tridibghosh12345@gmail.com>
2024-08-07 01:44:51 +0530

    File added

---
+++ JS-Programming-1/DataPrivacy.js
@@ -0,0 +1,15 @@
+function counter(){
+  var count=0
+   return function incrementCounter(){
+    count++
+    console.log(count)
+  }
+}
+
+var counter1=counter()
+counter1()
+counter1()
+counter1()
+counter1()
+counter1()
+
