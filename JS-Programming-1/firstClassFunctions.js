7a73ac2b4e44ffb7dedc3a159689e7c5502090e4
Tridib11 <tridibghosh12345@gmail.com>
2024-08-07 02:22:44 +0530

    File added

---
+++ JS-Programming-1/firstClassFunctions.js
@@ -0,0 +1,13 @@
+a()
+// b()
+//function statement aka function declaration
+function a(){
+  console.log("a called")
+}
+
+//function expression
+
+var b=function (){
+  console.log("b called")
+}
+
