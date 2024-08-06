b1d581384c322b70d230c82c2d4e068dce917423
Tridib11 <tridibghosh12345@gmail.com>
2024-08-06 17:13:48 +0530

    File added

--- JS-Programming-1/block.js
+++ JS-Programming-1/block.js
@@ -6,10 +6,15 @@
 
 console.log(a) // a can be accessed as it is  global 
 
-
+var z=10
 let t=10
 {
   let t=20
+  var z=100
   console.log(t)
+  console.log(z)
 
 }
+
+console.log(z) //100 var globally jinish take change kore
+console.log(t) //10 kintu let kore na
