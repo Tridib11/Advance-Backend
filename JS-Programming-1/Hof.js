111b97b9bada6db8d0c108242cf3be7683f942e8
Tridib11 <tridibghosh12345@gmail.com>
2024-08-10 16:22:38 +0530

    File added

--- JS-Programming-1/Hof.js
+++ JS-Programming-1/Hof.js
@@ -1,9 +1,16 @@
-function f(fx,fn){
-  console.log(fx)
-  fn()
-  console.log(fn)
-}
+// function f(fx,fn){
+//   console.log(fx)
+//   fn()
+//   console.log(fn)
+// }
 
-f(10,function exec(){
-  console.log("I am a higher order function")
+// f(10,function exec(){
+//   console.log("I am a higher order function")
+// })
+
+
+let b=[3,1,4,2,7,6]
+b.sort(function(a,b){
+  return a-b
 })
+console.log(b)
