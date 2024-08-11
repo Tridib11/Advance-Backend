881f898292cf29cacdac2c4e0642eafe5ca676af
Tridib11 <tridibghosh12345@gmail.com>
2024-08-12 01:44:41 +0530

    File added

--- JS-Programming-1/Promise.js
+++ JS-Programming-1/Promise.js
@@ -1,3 +1,26 @@
-function getRandomInt(max){
-  return Math.floor(Math.random()*max)
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
+
+function createPromiseWithTimeOut() {
+  return new Promise(function executor(resolve, reject) {
+    setTimeout(function () {
+      let num = getRandomInt(20);
+      if (num % 2 === 0) {
+        resolve(num);
+      } else {
+        reject(num);
+      }
+    }, 5000);
+  });
+}
+
+
+function output(num){
+  console.log(`The number is ${num}`)
+}
+
+
+
+createPromiseWithTimeOut().then(output)
+
