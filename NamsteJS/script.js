95dab9e8bca37a2a4f067d634464f4b060bb4a4e
Tridib11 <tridibghosh12345@gmail.com>
2024-08-21 23:02:59 +0530

    File added

--- NamsteJS/script.js
+++ NamsteJS/script.js
@@ -1,7 +1,15 @@
 // getName()
 // console.log(x)
-console.log(getName)
+// console.log(getName)
+
 var x=7;
+console.log(getName())
 function getName(){
   console.log("Namaste js")
 }
+
+
+
+getName()
+console.log(x)
+console.log(getName)
