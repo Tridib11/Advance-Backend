51739c0d69152bc459997e6e1adf607d041b1013
Tridib11 <tridibghosh12345@gmail.com>
2024-08-22 08:47:16 +0530

    File added

---
+++ NamsteJS/SetTImeout.js
@@ -0,0 +1,18 @@
+
+
+function x(){
+  for(var i=1;i<=5;i++){
+    setTimeout(()=>{
+      console.log(i)
+    },i*1000)
+  }
+}
+
+function x(){
+  for(let i=1;i<=5;i++){
+    setTimeout(()=>{
+      console.log(i)
+    },i*1000)
+  }
+}
+x()
