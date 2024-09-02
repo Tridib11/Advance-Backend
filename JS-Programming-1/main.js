d77b98629bbe628f83c84f2017ed873e9e898b9b
Tridib11 <tridibghosh12345@gmail.com>
2024-09-02 12:37:24 +0530

    File added

--- JS-Programming-1/main.js
+++ JS-Programming-1/main.js
@@ -1,3 +1,15 @@
 const allUsers=[{
-  
+  firstName:"Tridib",
+  gender:"Male",
+  age:21
+},{
+  firstName:"Rohan",
+  gender:"Male",
+  age:22
+},{
+  firstName:"Seeta",
+  gender:"Female",
+  age:25
 }]
+
+
